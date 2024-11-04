3425d6b01355cdb8f6e4e765b5bf4dbc45a46b0a
dongitschool <donavan.franco@utahtech.edu>
2024-11-04 18:02:15 +0000

    assignment11a done

---
+++ 11a/htmlfiles/water/config.php
@@ -0,0 +1,12 @@
+<?php
+/*************************
+ * EDIT THESE LINES
+ */
+$db_host = getenv('DB_HOST');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+$db_name = getenv('DB_NAME');
+/*
+ * EDIT THESE LINES
+ *************************/
+?>
